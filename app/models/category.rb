c03bed186c50291ef1aff3d7d6fb75aca1b0049e
Luisa Perez <luisa.perezw3@gmail.com>
2024-04-14 14:48:58 +0100

    adding expenses forms and adding expenses to Category amount total

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,16 @@
 class Category < ApplicationRecord
   belongs_to :user, optional: true
   validates :name, :amount, presence: true
+  has_many :expenses
+  before_create :initialize_total_amount
+
+  private
+
+  def initialize_total_amount
+    self.amount ||= 0
+  end
+
+  def update_category_amount_total
+    category.update(total_amount: category.sum(:amount))
+  end
 end
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,5 +1,13 @@
 class Expense < ApplicationRecord
   belongs_to :category, optional: true
+  after_create :update_category_total
   belongs_to :user, optional: true
   validates :title, :amount, :date,  presence: true
+
+  private
+
+  def update_category_total
+    category.update(amount: category.expenses.sum(:amount))
+  end
+
 end
