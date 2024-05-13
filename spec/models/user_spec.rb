8fdad1322062c5300e47dddce9965dfe6a98dab5
Luisa Perez <luisa.perezw3@gmail.com>
2024-05-13 20:25:46 +0100

    adding password test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe User, type: :model do
 
   before(:all) do
     @user_one = create(:user)
+    puts @user_one.inspect
   end
 
   it "is valid with valid attributes" do
@@ -15,16 +16,33 @@ RSpec.describe User, type: :model do
   end
 
   it "has a unique email" do
-    user_two = build(:user, email: "bob@gmail.com")
+    user_two = create(:user, email: "luisa@example.com")
+    puts user_two.inspect
     expect(user_two).to_not be_valid
   end
 
+  # Nil id for user_two if I use build. If I used the create will create an id but then the test does not pass. I believe it thinks is the same as user_one.
+
   it "is not valid without an email" do
-    user_two = build(:user, email: nil)
-    expect(user_two).to_not be_valid
+    user_three = build(:user, email: nil)
+    expect(user_three).to_not be_valid
+  end
+
+  it "is not valid without a password" do
+    user_without_password = build(:user, password: nil)
+    expect(user_without_password).to_not be_valid
+  end
+
+  it "has a minimum password length" do
+    user_with_short_password = build(:user, password: "123456")
+    expect(user_with_short_password).to_not be_valid
   end
 
 end
+
+
+
+
 
 
 # describe User do
@@ -36,27 +54,5 @@ end
 #     )
 #     expect(user.id).not_to be_nil
 #     expect(user.categories.count).not_to be_zero
-#   end
-# end
-
-# describe User do
-#   it "checks email is invalid" do
-#     user_two = User.new(
-#       email: "luisa@myevent.com",
-#       password: "123456",
-#       password_confirmation: "123456"
-#     )
-#     expect(user_two).not_to be_valid
-#   end
-# end
-
-# describe User do
-#   it "checks is unique" do
-#     user_three = User.new(
-#       email: "luisa@myevent.com",
-#       password: "123456",
-#       password_confirmation: "123456"
-#     )
-#     expect(user_three).not_to be_unique
 #   end
 # end
