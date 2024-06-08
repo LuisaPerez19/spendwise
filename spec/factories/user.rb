f53a19c59d6723b9e9bcab72df349e07512d6005
Luisa Perez <luisa.perezw3@gmail.com>
2024-06-08 13:02:10 +0100

    user factory update

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,10 +1,4 @@
 FactoryBot.define do
-  # factory :category do
-  #   name { "Test Category" }
-  #   amount { 0 }
-  #   user_id { 2 }
-  # end
-
 
   factory :user do
     sequence(:email) { |n| "dummyEmail#{n}@gmail.com" }
