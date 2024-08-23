78d0157839e032b7ba48a6609dfbfdaebd851af6
Luisa Perez <luisa.perezw3@gmail.com>
2024-08-23 20:19:01 +0100

    adding date_filter_stimulus_controller

--- app/javascript/controllers/date_filter_controller.js
+++ app/javascript/controllers/date_filter_controller.js
@@ -1,7 +1,24 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  static targets = ["dateFilter", "totalExpenses"];
   connect() {
     console.log("Hello, from your stimulus controller");
+    this.updateTotalExpenses();
   }
+
+  updateDateFilter(event) {
+    const selectedOption = event.target.value;
+    this.element.dispatchEvent(
+      new CustomEvent("date-filter-changed", { detail: { selectedOption } })
+    );
+    this.updateTotalExpenses();
+  }
+
+  updateTotalExpenses() {
+    const selectedOption = this.dateFilterTarget.value;
+    this.fechTotalExpenses(selectedOption);
+  }
+
+  fechTotalExpenses(dateRange) {}
 }
